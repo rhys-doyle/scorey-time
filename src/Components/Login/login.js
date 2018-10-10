7ca2b2e91a5a8b90a20de202594fa426357ed4f5
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-10 14:36:35 +1100

    environment setting

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -19,7 +19,7 @@ class Login extends React.Component {
         console.log("Received values of form", values);
       }
     });
-    fetch("http://localhost:3000/login", {
+    fetch(CREATE_REACT_API_URL, {
       method: "POST",
       body: JSON.stringify(this.state),
       headers: { "Content-Type": "application/json" }
