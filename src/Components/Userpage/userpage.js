071d02b94973ba632fff0110518a7672da2b6dd9
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-15 15:20:05 +1100

    userpage elements

--- src/Components/Userpage/userpage.js
+++ src/Components/Userpage/userpage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import profile from "/Users/developer/Documents/workspace/scorey-time/src/profilePhoto.jpg";
+import styles from "./userpage.css";
 
 export default class Userpage extends React.Component {
   render() {
-    return <div>userpage</div>;
+    return (
+      <div>
+        <div className={styles.div}>
+          <img src={profile} className={styles.img} alt="profile_photo" />
+        </div>
+      </div>
+    );
   }
 }
