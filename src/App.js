a1cc508b19ee372228489ed8868058fef3cf658f
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-18 20:36:04 +1100

    routing adjustment

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import Login from "./Components/Login/login";
 import Userpage from "./Components/Userpage/userpage";
 import Authentication from "./Components/Authentication/authentication";
+import Newgame from "./Components/Newgame/newgame";
 import styles from "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Redirect } from "react-router";
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className={styles.App}>
-          <Route exact path="/login" component={Login} />
           <Authentication>
-            <Route exact path="/userpage" component={Userpage} />
+            <Route path="/" exact render={() => <Redirect to="/userpage" />} />
+            <Route path="/userpage" component={Userpage} />
+            <Route path="/newgame" component={Newgame} />
           </Authentication>
+          <Route exact path="/login" component={Login} />
         </div>
       </BrowserRouter>
     );
