2155aed72862a73f0dfe2030d4a26c0e5e7e226d
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-19 11:02:04 +1100

    new game jsx and css

--- src/Components/Newgame/newgame.js
+++ src/Components/Newgame/newgame.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Button, Icon, Input } from "antd";
+import styles from "./newgame.css";
+const ButtonGroup = Button.Group;
 
 export default class Newgame extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className={styles.setup}>
+        <h1 className={styles.setupHeader}>Game Setup</h1>
+        <div className={styles.gameSetup}>
+          <ButtonGroup>
+            <Button type="primary" children="4" />
+            <Button type="primary" children="5" disabled />
+            <Button type="primary" children="6" />
+          </ButtonGroup>
+        </div>
+      </div>
+    );
   }
 }
