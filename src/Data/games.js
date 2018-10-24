e538736c26ac8fb546229db483c246e754486934
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-24 13:43:44 +1100

    games data

---
+++ src/Data/games.js
@@ -0,0 +1,56 @@
+const games = [
+  {
+    game: "500",
+    players: [
+      {
+        number: "4",
+        configuartion: ["2 teams of 2"],
+        teams: ["1&2 vs 3&4", "1&3 vs 2&4", "1&4 vs 2&3"]
+      },
+      {
+        number: "5",
+        configuration: ["5 teams of 1"],
+        teams: ["Individual Teams"]
+      },
+      {
+        number: "6",
+        configuration: ["2 teams of 3", "3 teams of 2"],
+        teams: [
+          [
+            "1&2&3 vs 4&5&6",
+            "1&2&4 vs 3&5&6",
+            "1&2&5 vs 3&4&6",
+            "1&2&6 vs 3&4&5",
+            "1&3&4 vs 2&5&6",
+            "1&3&5 vs 2&4&6",
+            "1&3&6 vs 2&4&5",
+            "1&4&5 vs 2&3&6",
+            "1&4&6 vs 2&3&5",
+            "1&5&6 vs 2&3&4"
+          ],
+          [
+            "1&2 vs 3&4 vs 5&6",
+            "1&2 vs 3&5 vs 4&6",
+            "1&2 vs 3&6 vs 4&5",
+            "1&3 vs 2&4 vs 5&6",
+            "1&3 vs 2&5 vs 4&6",
+            "1&3 vs 2&6 vs 4&5",
+            "1&4 vs 2&3 vs 5&6",
+            "1&4 vs 2&5 vs 3&6",
+            "1&4 vs 2&6 vs 3&5",
+            "1&5 vs 2&3 vs 4&6",
+            "1&5 vs 2&4 vs 3&6",
+            "1&5 vs 2&6 vs 3&4",
+            "1&6 vs 2&3 vs 4&5",
+            "1&6 vs 2&4 vs 3&5",
+            "1&6 vs 2&5 vs 3&6"
+          ]
+        ]
+      }
+    ]
+  },
+  "Euchre",
+  "Hearts"
+];
+
+export default games;
