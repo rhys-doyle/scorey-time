295e2533762f85d6668c230734700e2b0b7de7ea
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-25 12:52:59 +1100

    radio button and onchange handler

--- src/Components/Newgame/newgame.js
+++ src/Components/Newgame/newgame.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Button, Icon, Input } from "antd";
+import { Radio, Select, Button, Icon, Input } from "antd";
 import styles from "./newgame.css";
 import games from "../Data/games";
 import friends from "../Data/friends";
-const ButtonGroup = Button.Group;
 
 export default class Newgame extends React.Component {
+  state = {
+    players: 4
+  };
+
+  handleOnChange = event => {
+    this.setState({ players: event.target.value });
+  };
+
   render() {
+    const { players } = this.state;
     return (
       <div className={styles.setup}>
         <h1 className={styles.setupHeader}>Game Setup</h1>
@@ -16,8 +24,8 @@ export default class Newgame extends React.Component {
             children="GAME"
             className={styles.gameButton}
           />
-          <Input
-            list="games"
+          <Select
+            dataSource={friends}
             prefix={<Icon type="search" style={{ color: "#00000055" }} />}
             placeholder="Select Game"
             className={styles.gameSelect}
@@ -29,11 +37,15 @@ export default class Newgame extends React.Component {
             children="PLAYERS"
             className={styles.playerButton}
           />
-          <ButtonGroup className={styles.buttonBox}>
-            <Button type="default" children="4" />
-            <Button type="default" children="5" />
-            <Button type="default" children="6" />
-          </ButtonGroup>
+          <Radio.Group
+            value={players}
+            onChange={this.handleOnChange}
+            className={styles.buttonBox}
+          >
+            <Radio.Button value="4" children="4" />
+            <Radio.Button value="5" children="5" />
+            <Radio.Button value="6" children="6" />
+          </Radio.Group>
           <Input
             prefix={<Icon type="user" style={{ color: "#00000055" }} />}
             placeholder="Select Players"
@@ -46,18 +58,18 @@ export default class Newgame extends React.Component {
             type="primary"
             children="TEAMS"
           />
-          <ButtonGroup className={styles.teamsBox}>
-            <Button
+          <Radio.Group className={styles.teamsBox}>
+            <Radio.Button
               className={styles.teamsButton}
               type="default"
               children="2"
             />
-            <Button
+            <Radio.Button
               className={styles.teamsButton}
               type="default"
               children="3"
             />
-          </ButtonGroup>
+          </Radio.Group>
         </div>
       </div>
     );
