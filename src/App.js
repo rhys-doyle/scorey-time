cb4f6ef83d9beb5dbdde9c8f1657638ac308075d
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-30 13:18:14 +1100

    start game route

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default class App extends Component {
             <Route path="/" exact render={() => <Redirect to="/userpage" />} />
             <Route path="/userpage" component={Userpage} />
             <Route path="/newgame" component={Newgame} />
+            <Route path="/playgame" component={Playgame} />
           </Authentication>
           <Route exact path="/login" component={Login} />
         </div>
