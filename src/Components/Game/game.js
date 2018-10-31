c3e1ece0e38e178844348eae1c8c0de30df69cd7
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-31 17:02:42 +1100

    game component elements

--- src/Components/Game/game.js
+++ src/Components/Game/game.js
@@ -1,8 +1,27 @@
 import React from "react";
 import styles from "./game.css";
+import { Button, Slider, Select, Radio, Icon } from "antd";
 
 export default class Game extends React.Component {
   render() {
-    return <div className={styles.demoBox} children="Hello World!" />;
+    return (
+      <div className={styles.bidForm}>
+        Winning Bid
+        <div className={styles.teamBox}>
+          <Button
+            type="primary"
+            className={styles.teamButton}
+            children="Player"
+          />
+          <Select
+            placeholder='Select Player'
+            className={styles.playerSelect}
+            children={}
+        </div>
+        <div className={styles.suitBox} />
+        <div className={styles.tricksBox} />
+        <div className={styles.confirmBox} />
+      </div>
+    );
   }
 }
