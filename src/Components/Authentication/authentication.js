93cc184ce2d7a950a4eaaebe94429a44a00e2d80
Rhys Doyle <rhys.doyle@gmail.com>
2018-11-02 10:51:01 +1100

    fixing redirect logic

--- src/Components/Authentication/authentication.js
+++ src/Components/Authentication/authentication.js
@@ -3,8 +3,12 @@ import { Redirect } from "react-router";
 
 export default class Authentication extends React.Component {
   render() {
+    const { pathname } = this.props.history.location;
     var token = JSON.parse(localStorage.getItem("token"));
-    if (!token || token.expires < new Date().getTime()) {
+    if (
+      pathname !== "/login" &&
+      (!token || token.expires < new Date().getTime())
+    ) {
       return <Redirect to="/login" />;
     } else {
       return this.props.children;
