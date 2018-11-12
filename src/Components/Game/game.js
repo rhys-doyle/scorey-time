0a9dae1b0d01d2b062bb83b74d3c4d5ecae4074c
Rhys Doyle <rhys.doyle@gmail.com>
2018-11-12 14:27:54 +1100

    score sheet elements

--- src/Components/Game/game.js
+++ src/Components/Game/game.js
@@ -50,129 +50,138 @@ export default class Game extends React.Component {
 
   render() {
     return (
-      <div className={styles.bidForm}>
-        <div className={styles.titleBox}>
-          <h1 className={styles.titleHeader} children="Winning Bid" />
+      <div className={styles.biddingScoring}>
+        <div className={styles.bidForm}>
+          <div className={styles.titleBox}>
+            <h1 className={styles.titleHeader} children="Winning Bid" />
+          </div>
+          <div className={styles.handBox}>
+            {`Hand #${this.state.handNumber}`}
+            <div className={styles.borderBox} />
+          </div>
+          <div className={styles.teamBox}>
+            <Button
+              type="primary"
+              className={styles.teamButton}
+              children="PLAYER"
+            />
+            <Select
+              placeholder="Select Player"
+              value={this.state.biddingPlayer}
+              className={styles.playerSelect}
+              children={this.state.gameInfo[
+                this.state.gameInfo.length - 1
+              ].playerNames.map(name => (
+                <Select.Option key={name}>@{name}</Select.Option>
+              ))}
+              onChange={value => this.setState({ biddingPlayer: value })}
+            />
+          </div>
+          <div className={styles.suitBox}>
+            <Button
+              className={styles.suitBoxButton}
+              children="SUIT"
+              type="primary"
+            />
+            <Radio.Group
+              className={styles.suitRadio}
+              value={this.state.suit}
+              onChange={event => this.setState({ suit: event.target.value })}
+            >
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.blackSuit]: true
+                })}
+                value="Spades"
+                children="♠️"
+              />
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.blackSuit]: true
+                })}
+                value="Clubs"
+                children="♣️"
+              />
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.redSuit]: true,
+                  [styles.diamonds]: true
+                })}
+                value="Diamonds"
+                children="♦"
+              />
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.redSuit]: true
+                })}
+                value="Hearts"
+                children="♥︎"
+              />
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.noTrumps]: true
+                })}
+                disabled
+                value="No Trumps"
+                children="No Trumps"
+              />
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.misereButton]: true
+                })}
+                disabled
+                value="Misère"
+                children="Misère"
+              />
+              <Radio.Button
+                className={classnames({
+                  [styles.suitButton]: true,
+                  [styles.openButton]: true
+                })}
+                disabled
+                value="Open Misère"
+                children="Open Misère"
+              />
+            </Radio.Group>
+          </div>
+          <div className={styles.tricksBox}>
+            <Button
+              type="primary"
+              children="TRICKS"
+              className={styles.tricksButton}
+            />
+            <Slider
+              className={styles.tricksSlider}
+              max={10}
+              min={6}
+              step={1}
+              value={this.state.tricksNumber}
+              onChange={value => this.setState({ tricksNumber: value })}
+            />
+          </div>
+          <div className={styles.confirmBox}>
+            <Button
+              type="primary"
+              className={styles.confirmButton}
+              block
+              size="large"
+              onClick={this.handleConfirmBid}
+              children="CONFIRM WINNING BID"
+            />
+          </div>
         </div>
-        <div className={styles.handBox}>
-          {`Hand #${this.state.handNumber}`}
-          <div className={styles.borderBox} />
-        </div>
-        <div className={styles.teamBox}>
-          <Button
-            type="primary"
-            className={styles.teamButton}
-            children="PLAYER"
-          />
-          <Select
-            placeholder="Select Player"
-            value={this.state.biddingPlayer}
-            className={styles.playerSelect}
-            children={this.state.gameInfo[
-              this.state.gameInfo.length - 1
-            ].playerNames.map(name => (
-              <Select.Option key={name}>@{name}</Select.Option>
-            ))}
-            onChange={value => this.setState({ biddingPlayer: value })}
-          />
-        </div>
-        <div className={styles.suitBox}>
-          <Button
-            className={styles.suitBoxButton}
-            children="SUIT"
-            type="primary"
-          />
-          <Radio.Group
-            className={styles.suitRadio}
-            value={this.state.suit}
-            onChange={event => this.setState({ suit: event.target.value })}
-          >
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.blackSuit]: true
-              })}
-              value="Spades"
-              children="♠️"
-            />
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.blackSuit]: true
-              })}
-              value="Clubs"
-              children="♣️"
-            />
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.redSuit]: true,
-                [styles.diamonds]: true
-              })}
-              value="Diamonds"
-              children="♦"
-            />
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.redSuit]: true
-              })}
-              value="Hearts"
-              children="♥︎"
-            />
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.noTrumps]: true
-              })}
-              disabled
-              value="No Trumps"
-              children="No Trumps"
-            />
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.misereButton]: true
-              })}
-              disabled
-              value="Misère"
-              children="Misère"
-            />
-            <Radio.Button
-              className={classnames({
-                [styles.suitButton]: true,
-                [styles.openButton]: true
-              })}
-              disabled
-              value="Open Misère"
-              children="Open Misère"
-            />
-          </Radio.Group>
-        </div>
-        <div className={styles.tricksBox}>
-          <Button
-            type="primary"
-            children="TRICKS"
-            className={styles.tricksButton}
-          />
-          <Slider
-            className={styles.tricksSlider}
-            max={10}
-            min={6}
-            step={1}
-            value={this.state.tricksNumber}
-            onChange={value => this.setState({ tricksNumber: value })}
-          />
-        </div>
-        <div className={styles.confirmBox}>
-          <Button
-            type="primary"
-            className={styles.confirmButton}
-            block
-            size="large"
-            onClick={this.handleConfirmBid}
-            children="CONFIRM WINNING BID"
-          />
+        <div className={styles.scoreSheet}>
+          <div className={styles.titleBox}>
+            <h1 className={styles.titleHeader} children="Score Sheet" />
+          </div>
+
+          <div className={styles.separator} />
         </div>
       </div>
     );
