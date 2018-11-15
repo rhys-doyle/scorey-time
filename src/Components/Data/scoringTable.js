ef07396c1eadc56a597f50d4b82e6897c81307fe
Rhys Doyle <rhys.doyle@gmail.com>
2018-11-15 11:26:44 +1100

    score values

---
+++ src/Components/Data/scoringTable.js
@@ -0,0 +1,9 @@
+const scoringTable = {
+  Spades: [40, 140, 240, 340, 440],
+  Clubs: [60, 160, 260, 360, 460],
+  Diamonds: [80, 180, 280, 380, 480],
+  Hearts: [100, 200, 300, 400, 500],
+  "No Trumps": [120, 220, 320, 420, 520]
+};
+
+export default scoringTable;
